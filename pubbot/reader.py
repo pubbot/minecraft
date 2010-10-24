7800b67180911ab0c57a7b30d9cecbeaaa6c80c5
John Carr <john.carr@unrouted.co.uk>
2010-10-24 16:16:30 +0100

    Add a reader for NBT data, support byte_array, list and dict types

--- pubbot/reader.py
+++ pubbot/reader.py
@@ -1,35 +1,10 @@
 from struct import unpack
 from twisted.internet import defer
 
-class MinecraftReader(object):
+class BaseMinecraftReader(object):
 
-    def __init__(self):
-        self.data = ""
-        self.length_wanted = 0
-        self.callback = None
-
-    def dataReceived(self, data):
-        self.data += data
-        if self.callback and len(self.data) >= self.length_wanted:
-            callback, self.callback = self.callback, None
-            callback.callback(None)
-
-    @defer.inlineCallbacks
-    def read_raw(self, num_bytes):
-        """
-        I read x bytes from the server, if there isnt enough data then i yield a Deferred
-        effectively pausing the current code until the rest of the data is available
-        """
-        if len(self.data) < num_bytes:
-             # Not enough data: Pause this function until there is
-             self.length_wanted = num_bytes
-             self.callback = defer.Deferred()
-             yield self.callback
-
-        # Pop the data off the front of data and return it
-        data = self.data[0:num_bytes]
-        self.data = self.data[num_bytes:]
-        defer.returnValue(data)
+    def read_raw(self, size):
+        raise NotImplementedError(self.read_raw)
 
     @defer.inlineCallbacks
     def read_packet_id(self):
@@ -71,7 +46,7 @@ class MinecraftReader(object):
         length = yield self.read_short()
         data = yield self.read_raw(length)
         val = unpack(">%ds" % length, data)[0]
-        defer.returnValue(val)
+        defer.returnValue(val.decode("UTF-8"))
 
     @defer.inlineCallbacks
     def read_bool(self):
@@ -81,4 +56,88 @@ class MinecraftReader(object):
         else:
             defer.returnValue(False)
 
+
+class MinecraftReader(object):
+
+    def __init__(self):
+        self.length_wanted = 0
+        self.data = ""
+        self.callback = ""
+
+    def dataReceived(self, data):
+        self.data += data
+        if self.callback and len(self.data) >= self.length_wanted:
+            callback, self.callback = self.callback, None
+            callback.callback(None)
+
+    @defer.inlineCallbacks
+    def read_raw(self, num_bytes):
+        """
+        I read x bytes from the server, if there isnt enough data then i yield a Deferred
+        effectively pausing the current code until the rest of the data is available
+        """
+        if len(self.data) < num_bytes:
+             # Not enough data: Pause this function until there is
+             self.length_wanted = num_bytes
+             self.callback = defer.Deferred()
+             yield self.callback
+
+        # Pop the data off the front of data and return it
+        data = self.data[0:num_bytes]
+        self.data = self.data[num_bytes:]
+        defer.returnValue(data)
+
+
+class NBTReader(object):
+
+    self.tagmap = {
+        0x01: self.read_byte,
+        0x02: self.read_short,
+        0x03: self.read_int,
+        0x04: self.read_long,
+        0x05: self.read_float,
+        0x06: self.read_double,
+        0x07: self.read_byte_array,
+        0x08: self.read_string,
+        0x09: self.read_list,
+        0x0A: self.read_dict,
+        }
+
+    def __init__(self, fp):
+        self.fp = gzip.GzipFile(fileobj=fp, mode='rb')
+
+    def read_raw(self, num_bytes):
+        return defer.succeed(fp.read(num_bytes))
+
+    @defer.inlineCallbacks
+    def read_byte_array(self):
+        length = yield self.read_int()
+        data = []
+        for i in range(length):
+            data.append(yield self.read_byte())
+        defer.returnValue(data)
+
+    @defer.inlineCallbacks
+    def read_list(self):
+        tag = yield self.read_byte()
+        length = yield self.read_int()
+
+        data = []
+        for i in range(length):
+            data.append(yield self.tagmap[tag]())
+
+        defer.returnValue(data)
+
+    @defer.inlineCallbacks
+    def read_dict(self):
+        data = {}
+
+        while True:
+            tag = yield self.read_byte()
+            if tag == 0x00:
+                break
+            name = yield self.read_string()
+            data[name] = yield self.tagmap[tag]()
+
+        defer.returnValue(data)
 
