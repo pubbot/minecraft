114ebb555ac586fb31ecda01e49ee626ef312ee9
John Carr <john.carr@unrouted.co.uk>
2010-10-27 10:46:22 +0100

    Update griefing to exercise block placing

--- pubbot/activity.py
+++ pubbot/activity.py
@@ -1,9 +1,21 @@
 
 from pubbot import actions
+from pubbot.vector import Vector
 
 def grief(bot):
+    pos = bot.pos + Vector(1,1,1)
+
     say = actions.Say(bot, "Hello, is anybody there?")
-    wait = actions.Idle(bot, 600)
+    wait = actions.Idle(bot, 100)
+
+    build = actions.Build(bot, pos, 41)
+    say2 = actions.Say(bot, "I made a thing!")
+    wait2 = actions.Idle(bot, 100)
+
+    mine = actions.Dig(bot, pos)
+    say3 = actions.Say(bot, "My thing is gone :(")
+    wait3 = actions.Idle(bot, 100)
+
     repeat = actions.Functor(bot, grief, bot)
-    return (say, wait, repeat)
+    return (say, wait, build, say2, wait2, mine, say3, wait3, repeat)
 
