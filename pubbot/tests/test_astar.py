b300d9c527a8061c5e521ef0ca17e2a098a576ee
John Carr <john.carr@unrouted.co.uk>
2010-11-06 23:16:10 +0000

    Test simple hill

--- pubbot/tests/test_astar.py
+++ pubbot/tests/test_astar.py
@@ -54,3 +54,20 @@ class TestAStar(unittest.TestCase):
         self.failUnlessEqual(route[3], Vector(0, 0, 4))
         self.failUnlessEqual(route[4], Vector(0, 0, 5))
 
+    def test_hill(self):
+        world = MockWorld(
+            Vector(0,0,0), Vector(0,0,1), Vector(0,1,1),
+            Vector(0,1,2), Vector(0,1,3), Vector(0,1,4),
+            Vector(0,0,3), Vector(0,0,4), Vector(0,0,5))
+
+        route = path(world, Vector(0,0,0), [Vector(0,0,5)])
+
+        self.failUnlessEqual(len(route), 7)
+        self.failUnlessEqual(route[0], Vector(0, 0, 1))
+        self.failUnlessEqual(route[1], Vector(0, 1, 1))
+        self.failUnlessEqual(route[2], Vector(0, 1, 2))
+        self.failUnlessEqual(route[3], Vector(0, 1, 3))
+        self.failUnlessEqual(route[4], Vector(0, 1, 4))
+        self.failUnlessEqual(route[5], Vector(0, 0, 4))
+        self.failUnlessEqual(route[6], Vector(0, 0, 5))
+
