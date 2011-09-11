7881f77cb9c6f1ceb28a105805ed77cacb0c416b
John Carr <john.carr@isotoma.com>
2011-09-12 00:40:51 +0100

    Depend on Bravo

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version = '0.1',
     author = 'John Carr',
     author_email = 'john.carr@unrouted.co.uk',
-
-    zip_safe = True,
+    zip_safe = False,
     packages = ['pubbot', 'twisted.plugins'],
     install_requires = [
         'Twisted',
+        'Bravo',
     ],
     package_data={'twisted': ['plugins/pubbot.py']},
 )
